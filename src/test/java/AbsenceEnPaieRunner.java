36b16152a3cad72e9a41c213da6d47c11fcfa357
chebbi <chebbi@169.254.214.71>
2018-04-23 15:31:13 +0200

    el base

--- src/test/java/AbsenceEnPaieRunner.java
+++ src/test/java/AbsenceEnPaieRunner.java
@@ -8,5 +8,5 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @Cucumber.Options(format={"pretty","json:target/cucumber.json"},tags= "@ty")
-public class AbsenceEnPaieRunnerTest {
+public class AbsenceEnPaieRunner {
 }
